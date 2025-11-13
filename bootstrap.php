4aee83c74476e26065c063a13ce03d2ff8ed6a90
Loy <moranoseancarlo@gmail.com>
2025-11-13 11:00:07 +0800

    feat: added how the database class is used

--- bootstrap.php
+++ bootstrap.php
@@ -1,16 +1,16 @@
 <?php
-
+//bootstrap.php is used for preparing things in my application before it runs
 use Core\App;
 use Core\Container;
 use Core\Database;
 
 
-$container = new Container();
+$container = new Container(); //creates a new service container
 
-$container->bind('Core\Database', function(){
+$container->bind('Core\Database', function(){//binding database, or showing how to create a database class
     $config = require basePath('config.php');
 
     return new Database($config['database']);
 });
 
-App::setContainer($container);
+App::setContainer($container); //makes globally accessible throughout the application
